e4a8c69975c82f02e9ebbe0f79d0cf7b8d956382
BikashChand07 <155935410+BikashChand07@users.noreply.github.com>
2024-01-14 05:49:42 +0000

    function_basics

---
+++ 04_functions/01_function.js
@@ -0,0 +1,34 @@
+// function displayname()
+// {
+//     console.log("Bikash Chand")
+// }
+// displayname()
+
+// the varaibles or input used in function definition is called parameters
+//and the varaiables used in function call is called arguments
+
+// function showname(name)
+// {
+//     console.log(`My name is ${name}`)
+// }
+// showname("Bikash Chand")
+
+// function sum(num1,num2)
+// {
+//     console.log(`the sum of ${num1} and ${num2} is : ${num1 + num2}`)
+// }
+// sum(20,30)
+
+// function sum(num1,num2)
+// {
+//     let add=num1+num2;
+//     return add
+// }
+// let result=sum(23,7)
+// console.log("the sum is ",result)
+
+function sum(num1,num2)
+{
+    return `${num1 + num2}`
+} 
+console.log("the sum is ",sum(12,8))
